c445e93a7922c7cab77cbfa42f7929e75819d92f
omondi-mutuma <omondimutuma@gmail.com>
2024-08-21 15:08:51 +0300

    implement initial file and folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'custom-gradient-start': '#FFF',
+        'custom-gradient-end': '#B372CF',
+      },
     },
   },
   plugins: [],
